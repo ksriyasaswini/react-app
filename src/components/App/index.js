44337a8537cc97fa7dedf226774334b771d6ca9e
SriYasaswini <kandarpa.sriyasaswini@gmail.com>
2019-01-05 12:44:38 +0530

    create a form

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import Table from "./table/index";
 import Incrementer from "./incrementer/index";
 import Hello from "./hello";
 import View from "./view/index";
+import Form from "./form/index";
 
 import  {
     BrowserRouter as Router,
@@ -12,25 +13,37 @@ import  {
     Redirect,
 }from 'react-router-dom'
 
-const tableValues = [
-    ['101','Tony Stark', 'Iron Man', 'Avengers'],
-    ['102','Peter Parker', 'Spider Man', 'Avengers'],
-    ['103','Bruce Mayne', 'Bat Man', 'Justice League']
-];
 
 const tableHeaders=['Id','Name', 'Alias', 'Team'];
 
 
 class App extends React.Component {
 
-    state = {
-        selectedId: -1,
-        selectedRecord: {}
+    constructor(props) {
+        super(props)
+        this.createRecord = this.createRecord.bind(this)
     }
 
-    onViewClick(id) {
+    state = {
+        tableValues : [
+            ['101','Tony Stark', 'Iron Man', 'Avengers'],
+            ['102','Peter Parker', 'Spider Man', 'Avengers'],
+            ['103','Bruce Mayne', 'Bat Man', 'Justice League']
+        ]
+    }
+    createRecord(name, alias, team) {
+        console.log(name, alias , team)
+        const Id = (Math.random() * 100).toString()
+        const newRecord = [Id, name, alias, team]
+        //const newTableValues = this.state.tableValues.map(val => val)
+        const newTableValues = [...this.state.tableValues]
+        newTableValues.push(newRecord)
+        this.setState({tableValues: newTableValues})
+    }
+
+    /*onViewClick(id) {
         console.log(id)
-        const data = tableValues.find(val => val[0] ===id)
+        const data = this.state.tableValues.find(val => val[0] ===id)
                     const newRecord = {
                     name: data[1],
                     alias: data[2],
@@ -40,7 +53,7 @@ class App extends React.Component {
             selectedId: id,
             selectedRecord: newRecord
         })
-    }
+    }*/
     
     render() {
         return (
@@ -51,13 +64,14 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path="/list" render = { (props) => {
                         return <Table 
-                                    values = {tableValues} 
+                                    values = {this.state.tableValues} 
                                     headers = {tableHeaders} 
                                     history = {props.history}/>
                     }}/>
+
                     <Route exact path="/view/:id" render = { (props) => {
                         console.log(props)
-                        const data = tableValues.find(val => val[0] === props.match.params.id)
+                        const data = this.state.tableValues.find(val => val[0] === props.match.params.id)
                         const newRecord = {
                             name: data[1],
                             alias: data[2],
@@ -67,6 +81,14 @@ class App extends React.Component {
                                 name = {newRecord.name}
                                 alias = {newRecord.alias}
                                 team = {newRecord.team}/>
+                    }}/>
+
+                    <Route exact path="/create" render = { (props) => {
+                        console.log(props)
+                        
+                        return <Form formSubmitCallback = {this.createRecord}
+                                        history = {props.history}/>
+
                     }}/>
 
                     <Redirect to="/list" />
--- src/components/App/view/index.js
+++ src/components/App/view/index.js
@@ -1,16 +1,16 @@
 import React from "react"
-import Form from "../form/index";
+import Form from "../form/index"
 
 
 class View extends React.Component {
     render() {
         return(
             <section>
+                
                 <h3>View Details</h3>
                 <div>Name: {this.props.name}</div>
                 <div>Alias: {this.props.alias}</div>
                 <div>Team: {this.props.team}</div>
-                <Form/>
             </section>
         )
     }
